dddf7a5c36c56418f4e75aee26782d22e722a0f1
souzagio <eusougiovany@gmail.com>
2023-08-30 13:35:44 -0300

    SplashForm

---
+++ Options/SplashLogin.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Supermarket.Options
+{
+    partial class SplashLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.metroProgressBar1 = new MetroFramework.Controls.MetroProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(513, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(118, 108);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // metroProgressBar1
+            // 
+            this.metroProgressBar1.HideProgressText = false;
+            this.metroProgressBar1.Location = new System.Drawing.Point(23, 87);
+            this.metroProgressBar1.Name = "metroProgressBar1";
+            this.metroProgressBar1.Size = new System.Drawing.Size(472, 23);
+            this.metroProgressBar1.Style = MetroFramework.MetroColorStyle.Purple;
+            this.metroProgressBar1.TabIndex = 1;
+            this.metroProgressBar1.UseWaitCursor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // SplashLogin
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 133);
+            this.Controls.Add(this.metroProgressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "SplashLogin";
+            this.Style = MetroFramework.MetroColorStyle.Purple;
+            this.Text = "Carregando. . .";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Options/SplashLogin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Supermarket.Options
+{
+    public partial class SplashLogin : MetroFramework.Forms.MetroForm
+    {
+        public SplashLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            metroProgressBar1.Value = metroProgressBar1.Value + 1;
+
+           /* if (metroProgressBar1.Value >= 100)
+            {
+                Perfil perf = new Perfil();
+                int x = this.Left + (this.Width / 2) - (perf.Width / 2);
+                int y = this.Top + (this.Height / 2) - (perf.Height / 2);
+                perf.Show();
+                perf.Location = new Point(x, y);
+                this.Close();
+            }*/
+        }
+    }
+}
